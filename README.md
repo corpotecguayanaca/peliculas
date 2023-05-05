41ef07f726ee5ae0b1102a76cd9efcebd8e5af70
Luis Marquez <luisjose0527@gmail.com>
2023-05-05 13:59:50 -0400

    First Commit. Creando el proyecto

---
+++ README.md
@@ -0,0 +1,5 @@
+# peliculas
+
+Esta es una aplicacion funcional que muestra un catalogo de peliculas actualizado. En ella se manejan peticiones HTTP.
+
+Este repositorio es parte de una serie de repositorios con aplicaciones de demostracion para aprender a usar el framework Flutter.
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/pages/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Peliculas',
+      initialRoute: '/',
+      routes: {
+        '/':(context) => HomePage(),
+      },
+    );
+  }
+}
